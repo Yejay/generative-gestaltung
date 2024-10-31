36e9dce8443ff0e84d8b2313a1cf0122c5eb6af7
Yejay Mac <demirkan.yejay@gmail.com>
2024-10-31 13:11:00 +0100

    deleted comments

--- Aufgabe_2/particle.js
+++ Aufgabe_2/particle.js
@@ -1,78 +1,10 @@
-// // Daniel Shiffman
-// // http://codingtra.in
-// // http://patreon.com/codingtrain
-// // Code for: https://youtu.be/BjoM9oKOAKY
-
-// class Particle {
-//     constructor() {
-//       this.pos = createVector(random(width), random(height));
-//       this.vel = createVector(0, 0);
-//       this.acc = createVector(0, 0);
-//       this.maxspeed = 4;
-//       this.prevPos = this.pos.copy();
-//     }
-  
-//     update() {
-//       this.vel.add(this.acc);
-//       this.vel.limit(this.maxspeed);
-//       this.pos.add(this.vel);
-//       this.acc.mult(0);
-//     }
-  
-//     follow(vectors) {
-//       var x = floor(this.pos.x / scl);
-//       var y = floor(this.pos.y / scl);
-//       var index = x + y * cols;
-//       var force = vectors[index];
-//       this.applyForce(force);
-//     }
-  
-//     applyForce(force) {
-//       this.acc.add(force);
-//     }
-  
-//     show() {
-//       stroke(255, 10);
-//       strokeWeight(1);
-//       line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
-//       this.updatePrev();
-//     }
-  
-//     updatePrev() {
-//       this.prevPos.x = this.pos.x;
-//       this.prevPos.y = this.pos.y;
-//     }
-  
-//     edges() {
-//       if (this.pos.x > width) {
-//         this.pos.x = 0;
-//         this.updatePrev();
-//       }
-//       if (this.pos.x < 0) {
-//         this.pos.x = width;
-//         this.updatePrev();
-//       }
-//       if (this.pos.y > height) {
-//         this.pos.y = 0;
-//         this.updatePrev();
-//       }
-//       if (this.pos.y < 0) {
-//         this.pos.y = height;
-//         this.updatePrev();
-//       }
-  
-//     }
-  
-//   }
-
-// TODO
 class Leaf {
     constructor() {
       // Initialize leaf properties
       this.pos = createVector(random(width), random(-50, 0));
       this.vel = createVector(0, 0);
       this.acc = createVector(0, 0);
-      this.maxspeed = random(0., 1);  // Adjust leaf speed range here
+      this.maxspeed = random(1, 5);  // Adjust leaf speed range here
       this.color = color(random([
         '#e63946',  // Red
         '#f4a261',  // Orange
@@ -88,7 +20,7 @@ class Leaf {
       this.vel.limit(this.maxspeed);
       this.pos.add(this.vel);
       this.acc.mult(0);
-      this.acc.add(createVector(0, 1.5));  // Adjust gravity here
+      this.acc.add(createVector(0, .5));  // Adjust gravity here
     }
   
     follow(vectors) {
@@ -120,6 +52,7 @@ class Leaf {
       pop();
     }
   
+    // TODO
     edges() {
       // Reset leaf position when it goes off screen
       const groundHeight = 0;  // If rect aka ground is visible in sketch.js, set this to 50 otherwise 0
--- Aufgabe_2/sketch.js
+++ Aufgabe_2/sketch.js
@@ -1,68 +1,3 @@
-// // Daniel Shiffman
-// // https://thecodingtrain.com/CodingChallenges/024-perlinnoiseflowfield.html
-
-// var inc = 0.1;
-// var scl = 10;
-// var cols, rows;
-
-// var zoff = 0;
-
-// var fr;
-
-// var particles = [];
-
-// var flowfield;
-
-// function setup() {
-//   createCanvas(600, 400);
-//   cols = floor(width / scl);
-//   rows = floor(height / scl);
-//   fr = createP('');
-
-//   flowfield = new Array(cols * rows);
-
-//   for (var i = 0; i < 300; i++) {
-//     particles[i] = new Particle();
-//   }
-//   background(51);
-// }
-
-// function draw() {
-//   var yoff = 0;
-//   for (var y = 0; y < rows; y++) {
-//     var xoff = 0;
-//     for (var x = 0; x < cols; x++) {
-//       var index = x + y * cols;
-//       var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
-//       var v = p5.Vector.fromAngle(angle);
-//       v.setMag(1);
-//       flowfield[index] = v;
-//       xoff += inc;
-//       stroke(0, 50);
-//       // push();
-//       // translate(x * scl, y * scl);
-//       // rotate(v.heading());
-//       // strokeWeight(1);
-//       // line(0, 0, scl, 0);
-//       // pop();
-//     }
-//     yoff += inc;
-
-//     zoff += 0.0003;
-//   }
-
-//   for (var i = 0; i < particles.length; i++) {
-//     particles[i].follow(flowfield);
-//     particles[i].update();
-//     particles[i].edges();
-//     particles[i].show();
-//   }
-
-//   // fr.html(floor(frameRate()));
-// }
-
-
-// TODO
 // Configuration variables
 let inc = 0.1;
 let scl = 20;
@@ -71,8 +6,8 @@ let zoff = 0;
 let particles = [];
 let flowfield;
 let bgImage; // Variable to store the background image
-let canvasWidth = 600;
-let canvasHeight = 400;
+let canvasWidth = 1000;
+let canvasHeight = 1000;
 
 function preload() {
   // Load the background image
@@ -105,7 +40,7 @@ function draw() {
       let index = x + y * cols;
       let angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
       let v = p5.Vector.fromAngle(angle);
-      v.setMag(0.5);
+      v.setMag(0.3);
       flowfield[index] = v;
 
       // Visualize flow field (optional)
@@ -128,9 +63,4 @@ function draw() {
     particles[i].edges();
     particles[i].show();
   }
-
-  // Draw ground (optional, depending on your background image)
-  // noStroke();
-  // fill('rgba(101, 67, 33, 0.5)');  // Semi-transparent brown
-  // rect(0, height - 50, width, 50);
 }
