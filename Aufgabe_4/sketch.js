38b89d90d593aa6afff5c343836e84e03cddc875
Vasiliki Ioannidou <vasiliki.ioannidou@siemens.com>
2024-11-24 16:28:24 +0100

    Fireworks with buildings - have to make it even fancier

--- Aufgabe_4/sketch.js
+++ Aufgabe_4/sketch.js
@@ -1,245 +1,178 @@
-let fireworks = [];
-let explosionParticles = [];
+// sketch.js
+const fireworks = [];
+const buildings = [];
+const stars = [];
 let gravity;
-let stars = [];
-let explosionSound, launchSound;
-let soundsLoaded = false;
-
-function preload() {
-  explosionSound = loadSound('https://assets.mixkit.co/active_storage/sfx/1647/1647.wav', 
-    () => {
-      explosionSound.setVolume(0.3);
-      soundsLoaded = true;
-    }
-  );
-  launchSound = loadSound('https://assets.mixkit.co/active_storage/sfx/2873/2873.wav',
-    () => {
-      launchSound.setVolume(0.2);
-      soundsLoaded = true;
-    }
-  );
-}
+let lastLightToggle = 0;
+let lastStarBlink = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  colorMode(HSB, 360, 100, 100, 1);
+  colorMode(HSB);
   gravity = createVector(0, 0.2);
-  initializeStars();
+  stroke(255);
+  strokeWeight(4);
+  background(0);
+
+  // Generate buildings
+  const buildingWidth = width / 6;
+  for (let i = 0; i < 6; i++) {
+    const x = i * buildingWidth + random(0, buildingWidth / 4);
+    const w = random(buildingWidth / 2, buildingWidth * 0.8);
+    const h = random(height / 4, height / 2);
+    buildings.push(new Building(x, height - h, w, h));
+  }
+
+  // Generate stars
+  for (let i = 0; i < 100; i++) {
+    stars.push(new Star(random(width), random(height / 2), random(1, 3)));
+  }
 }
 
 function draw() {
-  background(240, 20, 5, 0.2);
-  drawStars();
-  
-  if (random(1) < 0.03) {
-    launchFirework();
-  }
-  
-  updateAndDisplayFireworks();
-  updateAndDisplayExplosions();
-}
+  colorMode(RGB);
+  background(0, 0, 0, 25);
 
-function launchFirework() {
-  let x = random(width * 0.1, width * 0.9);
-  let targetHeight = random(height * 0.2, height * 0.6);
-  fireworks.push(new Firework(x, height, targetHeight));
-  launchSound.play();
-}
-
-function initializeStars() {
-  for (let i = 0; i < 300; i++) {
-    stars.push({
-      pos: createVector(random(width), random(height)),
-      brightness: random(0.3, 1),
-      size: random(1, 2.5),
-      twinkleSpeed: random(0.02, 0.08)
-    });
-  }
-}
-
-function drawStars() {
-  noStroke();
+  // Draw stars
   for (let star of stars) {
-    let twinkle = sin(frameCount * star.twinkleSpeed) * 0.3 + 0.7;
-    fill(0, 0, 100, star.brightness * twinkle);
-    circle(star.pos.x, star.pos.y, star.size);
+    star.show();
   }
-}
 
-function updateAndDisplayFireworks() {
+  // Blink stars every 3 seconds
+  if (millis() - lastStarBlink > 3000) {
+    for (let i = 0; i < random(1, 3); i++) {
+      random(stars).blink();
+    }
+    lastStarBlink = millis();
+  }
+
+  // Toggle building lights every 4 seconds
+  if (millis() - lastLightToggle > 4000) {
+    for (let building of buildings) {
+      building.toggleLights();
+    }
+    lastLightToggle = millis();
+  }
+
+  // Draw and update buildings
+  for (let building of buildings) {
+    building.show();
+  }
+
+  // Randomly launch fireworks from different positions
+  if (random(1) < 0.04) {
+    // Launch from building tops
+    const building = random(buildings);
+    fireworks.push(new Firework(building.x + random(0, building.w), building.y, "foreground"));
+
+    // Launch from the bottom edge
+    if (random(1) < 0.02) {
+      fireworks.push(new Firework(random(width), height, random(["foreground", "background"])));
+    }
+
+    // Launch from the left edge
+    if (random(1) < 0.01) {
+      fireworks.push(new Firework(0, random(height), "foreground"));
+    }
+
+    // Launch from the right edge
+    if (random(1) < 0.01) {
+      fireworks.push(new Firework(width, random(height), "foreground"));
+    }
+  }
+
+  // Update and display fireworks
   for (let i = fireworks.length - 1; i >= 0; i--) {
-    let fw = fireworks[i];
-    fw.update();
-    fw.display();
-    
-    if (fw.isDead()) {
-      createExplosionParticles(fw.pos.x, fw.pos.y, fw.color, fw.type);
-      explosionSound.play();
+    fireworks[i].update();
+    fireworks[i].show();
+
+    if (fireworks[i].done()) {
       fireworks.splice(i, 1);
     }
   }
 }
 
-function updateAndDisplayExplosions() {
-  for (let i = explosionParticles.length - 1; i >= 0; i--) {
-    let p = explosionParticles[i];
-    updateExplosionParticle(p);
-    displayExplosionParticle(p);
-    
-    if (p.life <= 0) {
-      explosionParticles.splice(i, 1);
-    }
+// Building class
+class Building {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.windowSize = 10;
+    this.windowPadding = 5;
+    this.lights = [];
+    this.initializeLights();
   }
-}
 
-class Firework {
-  constructor(x, y, targetHeight) {
-    this.pos = createVector(x, y);
-    this.vel = createVector(random(-1, 1), -sqrt(2 * gravity.y * (y - targetHeight)));
-    this.acc = createVector(0, 0);
-    this.color = random(360);
-    this.trail = [];
-    this.dead = false;
-    this.type = floor(random(5));
-  }
-  
-  update() {
-    this.acc.add(gravity);
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
-    this.acc.mult(0);
-    
-    this.trail.push(this.pos.copy());
-    if (this.trail.length > 15) {
-      this.trail.shift();
-    }
-    
-    if (this.vel.y > 0) {
-      this.dead = true;
+  initializeLights() {
+    const rows = Math.floor((this.h - this.windowPadding) / (this.windowSize + this.windowPadding));
+    const cols = Math.floor((this.w - this.windowPadding) / (this.windowSize + this.windowPadding));
+    for (let i = 0; i < rows; i++) {
+      this.lights[i] = [];
+      for (let j = 0; j < cols; j++) {
+        this.lights[i][j] = random() < 0.5;
+      }
     }
   }
-  
-  display() {
-    noFill();
-    for (let i = 1; i < this.trail.length; i++) {
-      let alpha = map(i, 0, this.trail.length, 0, 1);
-      strokeWeight(map(i, 0, this.trail.length, 1, 4));
-      stroke(this.color, 80, 100, alpha);
-      line(this.trail[i-1].x, this.trail[i-1].y, 
-           this.trail[i].x, this.trail[i].y);
+
+  toggleLights() {
+    for (let i = 0; i < this.lights.length; i++) {
+      for (let j = 0; j < this.lights[i].length; j++) {
+        if (random(1) < 0.5) {
+          this.lights[i][j] = !this.lights[i][j];
+        }
+      }
     }
-    
+  }
+
+  show() {
+    // Draw building
+    fill(50);
     noStroke();
-    fill(this.color, 80, 100, 0.8);
-    circle(this.pos.x, this.pos.y, 6);
-  }
-  
-  isDead() {
-    return this.dead;
-  }
-}
+    rect(this.x, this.y, this.w, this.h);
 
-function createExplosionParticles(x, y, baseColor, type) {
-  let particleCount = random(100, 200);
-  
-  for (let i = 0; i < particleCount; i++) {
-    let angle, speed;
-    
-    switch(type) {
-      case 0: // Circular
-        angle = random(TWO_PI);
-        speed = random(5, 15);
-        break;
-      case 1: // Heart shape
-        angle = random(TWO_PI);
-        let r = 16 * pow(sin(angle), 3);
-        speed = r * random(0.5, 1.5);
-        break;
-      case 2: // Double spiral
-        angle = random(TWO_PI);
-        speed = angle * 0.5 + random(5, 10);
-        break;
-      case 3: // Star pattern
-        angle = floor(random(5)) * TWO_PI / 5 + random(-0.2, 0.2);
-        speed = random(8, 16);
-        break;
-      case 4: // Ring with trails
-        angle = random(TWO_PI);
-        speed = 12 + random(-2, 2);
-        break;
-    }
-    
-    let vel = p5.Vector.fromAngle(angle).mult(speed);
-    let colorVariation = random(-20, 20);
-    let finalColor = (baseColor + colorVariation + 360) % 360;
-    
-    explosionParticles.push({
-      pos: createVector(x, y),
-      vel: vel,
-      color: finalColor,
-      life: 255,
-      hasTail: random() < 0.3,
-      trail: [],
-      glowSize: random(2, 6),
-      decay: random(1.5, 3),
-      canSplit: random() < 0.2
-    });
-  }
-}
-
-function updateExplosionParticle(p) {
-  p.vel.add(gravity);
-  p.pos.add(p.vel);
-  p.life -= p.decay;
-  
-  if (p.hasTail) {
-    p.trail.push(p.pos.copy());
-    if (p.trail.length > 10) p.trail.shift();
-  }
-  
-  if (p.canSplit && p.life < 128 && random() < 0.02) {
-    let splitCount = floor(random(3, 6));
-    for (let j = 0; j < splitCount; j++) {
-      explosionParticles.push({
-        pos: p.pos.copy(),
-        vel: p5.Vector.random2D().mult(random(2, 5)),
-        color: (p.color + random(-40, 40) + 360) % 360,
-        life: p.life * 0.8,
-        hasTail: true,
-        trail: [],
-        glowSize: p.glowSize * 0.7,
-        decay: p.decay * 1.2,
-        canSplit: false
-      });
-    }
-    p.life = 0;
-  }
-}
-
-function displayExplosionParticle(p) {
-  if (p.hasTail) {
-    noFill();
-    for (let j = 1; j < p.trail.length; j++) {
-      let alpha = map(j, 0, p.trail.length, 0, p.life/255);
-      stroke(p.color, 80, 100, alpha);
-      strokeWeight(2);
-      line(p.trail[j-1].x, p.trail[j-1].y,
-           p.trail[j].x, p.trail[j].y);
+    // Draw windows
+    for (let i = 0; i < this.lights.length; i++) {
+      for (let j = 0; j < this.lights[i].length; j++) {
+        const lightOn = this.lights[i][j];
+        const x = this.x + j * (this.windowSize + this.windowPadding) + this.windowPadding;
+        const y = this.y + i * (this.windowSize + this.windowPadding) + this.windowPadding;
+        fill(lightOn ? color(255, 204, 0) : color(30));
+        rect(x, y, this.windowSize, this.windowSize);
+      }
     }
   }
-  
-  noStroke();
-  for (let j = 3; j > 0; j--) {
-    fill(p.color, 80, 100, (p.life/255) / j);
-    circle(p.pos.x, p.pos.y, p.glowSize * j);
+}
+
+// Star class
+class Star {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.blinking = false;
+    this.blinkTimer = 0;
   }
-}
 
-function mousePressed() {
-  fireworks.push(new Firework(mouseX, height, mouseY));
-  launchSound.play();
-}
+  blink() {
+    this.blinking = true;
+    this.blinkTimer = millis();
+  }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  show() {
+    fill(255);
+    noStroke();
+
+    if (this.blinking) {
+      const elapsed = millis() - this.blinkTimer;
+      if (elapsed > 500) {
+        this.blinking = false;
+      } else {
+        fill(255, 255 - (elapsed / 500) * 255);
+      }
+    }
+
+    ellipse(this.x, this.y, this.size, this.size);
+  }
 }
